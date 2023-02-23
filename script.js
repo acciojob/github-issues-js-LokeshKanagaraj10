82689f3bea551692f58096756fa76e8b72294064
LokeshKanagaraj10 <120785781+LokeshKanagaraj10@users.noreply.github.com>
2023-02-23 15:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your code here
+const pageNumber = document.getElementByID("pageNumber")
+async function fetchdata(PageNumberHere){
+	const response = await fetch (`https://api.github.com/repositories/1296269/issues?page=${pageNumberHere}&per_page=5`)
+	const data = response.json();
+	console.log(data);
+}
+
+function loadNext(){
+	pageNumber++;
+	fetch(pageNumber)
+	pageNumber.innerText = pageNumber;
+}
+
+function loadPrev(){
+	pageNumber--;
+	if(pageNumber === 1){
+		document.getElementById("load_prev").disable = true;
+	}
+	else{
+		document.getElementById("load_prev").disable = false;
+		fetch(pageNumber)
+		pageNumber.innerText = pageNumber;
+	}
+	
+}
